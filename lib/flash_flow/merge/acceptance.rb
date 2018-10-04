888b9b2cf4cb24a59aaeeabb781db8673ef9dc10
Brad Bennett <brad.bennett@daqri.com>
2018-10-03 17:19:40 -0700

    Fix call broken when method signature changed

--- lib/flash_flow/merge/acceptance.rb
+++ lib/flash_flow/merge/acceptance.rb
@@ -102,7 +102,7 @@ module FlashFlow
         raise OutOfSyncWithRemote.new("Your branch is out of sync with the remote. If you want to force push, run 'flash_flow -f'") unless @local_git.last_success?
 
         if @do_not_merge
-          @data.remove_from_merge(@local_git.remote, @local_git.working_branch)
+          @data.remove_from_merge(@local_git.working_branch)
         else
           @data.add_to_merge(@local_git.working_branch)
         end
